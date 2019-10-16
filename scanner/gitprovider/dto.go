af2c9beb5e684445e2da656893d7e040526d276c
Jonathan Yang <jonathan.yang@grabtaxi.com>
2019-10-16 18:12:27 +0800

    add copyright header to source codes and license file

--- scanner/gitprovider/dto.go
+++ scanner/gitprovider/dto.go
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2019 Grabtaxi Holdings PTE LTE (GRAB), All rights reserved.
+ * Use of this source code is governed by an MIT-style license that can be found in the LICENSE file
+ */
+
 package gitprovider
 
 // Repository is a universal struct for holding repo info fields
